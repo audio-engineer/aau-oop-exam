09a4abdfe460fb7fe364eaa7157ef2515f306edd
Martin Kedmenec <martin.kedmenec@outlook.com>
2025-01-20 08:45:19 +0100

    Initial commit

---
+++ src/main/java/dk/aau/oop/exam/Application.java
@@ -0,0 +1,11 @@
+package dk.aau.oop.exam;
+
+final class Application {
+  private Application() {
+    //
+  }
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
